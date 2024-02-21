3e7fdd2d8d428997732fec6b3187170ee2d3538c
Anurag Tripathi <113921508+anurag24-26@users.noreply.github.com>
2024-02-21 22:09:02 +0530

    Create Factorial.js

---
+++ Factorial.js
@@ -0,0 +1,10 @@
+let num=prompt("enter a number for its factorial",1);
+let factori=num;
+for (let i = num-1; i >=1; i--) {
+    factori =i *factori;
+}
+alert(`The Factorial of ${num} is ` + factori);
+
+
+/*Simple program to print factorial of a given number
+i.e.if the number in the prompt is 5 it will give output 120. */
