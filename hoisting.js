740fd5179c6726020d2c6fc6cc3ec349ea7c2249
Anurag Tripathi <113921508+anurag24-26@users.noreply.github.com>
2024-06-10 23:12:02 +0530

    Create hoisting.js

---
+++ hoisting.js
@@ -0,0 +1,16 @@
+//HOisting in js is something like if i i declare a function in a variable and call the function before the declaration. example is below
+addone(8)
+
+function addone(num){
+    return num+1
+}
+
+addtwo(8)
+const addtwo=function(num){
+    return num+2
+}
+/*addtwo(8)
+^
+
+ReferenceError: Cannot access 'addtwo' before initialization
+    */
